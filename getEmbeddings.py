8be57706e02dfaced306de3071ab087f7d3da8c1
mannydoorman <147976825+mannydoorman@users.noreply.github.com>
2025-05-28 09:09:14 -0700

    Commenting

--- getEmbeddings.py
+++ getEmbeddings.py
@@ -3,5 +3,5 @@ from langchain_huggingface import HuggingFaceEmbeddings
 
 def getEmbeddings():
     embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-mpnet-base-v2")
+        model_name="sentence-transformers/all-mpnet-base-v2") #can be changed to any other model
     return embeddings
